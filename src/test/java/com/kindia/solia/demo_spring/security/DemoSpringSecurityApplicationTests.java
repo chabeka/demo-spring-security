90902a18e5c7bfced2ee6bf06ffef557186a859f
Diallo Mamadou-Aliou <mamadou-aliou.diallo@inetum.com>
2024-10-21 19:24:49 +0200

    first commit after initialize projet

---
+++ src/test/java/com/kindia/solia/demo_spring/security/DemoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kindia.solia.demo_spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
